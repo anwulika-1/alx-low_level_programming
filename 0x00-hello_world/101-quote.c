610fb4479215d35a14d560b41330c502ae9c7021
anwulika-1 <anwulikahappinex@gmail.com>
2022-09-08 18:18:03 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
  *
- * Return: Always 0 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-	return (1);
+        write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
