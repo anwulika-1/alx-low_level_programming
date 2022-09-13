d6717fbe0d38061ce5926517e541faf0227bee70
anwulika-1 <anwulikahappinex@gmail.com>
2022-09-13 10:22:01 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,23 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Prints _putchar output as a message.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+
+int main(void)
+  
+{       int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+        int count, 5z;
+
+	5s = sizeof(str) / sizeof(int);
+	for (count = 0; count < 5z; count++)
+	{
+	        _putchar(str(count]);
+	}
+	
+	_putchar('\n');
+	return (0);
 }
+	  
