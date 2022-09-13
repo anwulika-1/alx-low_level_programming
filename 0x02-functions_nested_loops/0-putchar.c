dcf40a7b332cd62800e3c7ead595b31c8eadc9e2
anwulika-1 <anwulikahappinex@gmail.com>
2022-09-13 13:52:20 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
 /**
- * main - Prints _putchar as a message.
- *
- * Return: Always 0 (Success)
+ * main - Print "Holberton" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
  */
-
 int main(void)
 {
-        int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-  
-        sz = sizeof(str) / sizeof(int);
-        for (count = 0; count < sz; count++)
-	{
-	          _putchar(str[count]);
-        }
-  
-        _putchar('\n');
-	return (0);
+int c = 0;
+char holberton[10] = "_putchar\n";
+
+while (c < 9)
+{
+_putchar(holberton[c]);
+c++;
 }
+return (0);
+}
+
 
