3f3a7bcc53787c3b945a85f9dbd00a5df885a9aa
anwulika-1 <anwulikahappinex@gmail.com>
2022-09-28 07:11:03 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: the string to print
  */
 void _print_rev_recursion(char *s)
 {
-       if (*s)
-       {
-	 _print_rev_recursion(s + 1);
-	 _putchar(*s);
-       }
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
