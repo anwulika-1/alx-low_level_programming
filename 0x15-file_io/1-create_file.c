843ea7d655e93e61555873bdea1ec8602fa67693
anwulika-1 <anwulikahappinex@gmail.com>
2022-10-31 07:54:17 -0700

    0x15 Task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,28 @@
 #include "main.h"
 /**
-  * create_file - creates a file.
-  * @filename: pointer to filename.
-  * @text_content: string to be written to file.
-  *
-  * Return:  1 on success -1 on failure.
+  * create_file - creates a file with rw------- permissions
+  * @filename: name of the file, if NULL, return -1
+  * @text_content: contents of the file. If NULL, create an empty file
+  * Return: 1 on success, -1 on failure
   */
 int create_file(const char *filename, char *text_content)
 {
-	int file, i, wr;
+	int new_file, len, wr_stat;
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (file == -1)
+	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (new_file == -1)
 		return (-1);
-
-	if (text_content)
+	if (text_content == NULL)
 	{
-		i = 0;
-		while (text_content[i])
-			i++;
-		wr = write(file, text_content, i);
-		if (wr != i)
-			return (-1);
+		close(new_file);
+		return (1);
 	}
-
-
+	for (len = 0; text_content[len]; len++)
+		;
+	wr_stat = write(new_file, text_content, len);
+	if (close(new_file) == -1)
+		return (-1);
+	return (wr_stat == -1 ? -1 : 1);
+}
--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,29 +1,11 @@
-#include <elf.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
-
+#include "mainn.h"
 /**
- * closes_file - Closes a file.
- * @fil: ELF file.
- * Return: none.
- */
-
-void closes_file(int fil)
+  * placeholder2 - define function
+  * @void: describe argument
+  * Return: 0 on success
+  */
+int placeholder2(void)
 {
-if (close(fil) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Cannot close fd %d\n", fil);
-exit(98);
+	return (0);
 }
-}
-
-/**
- * check_file - Checks if file is ELF.
- * @rem: Remaining file content.
- * Return: none.
 
